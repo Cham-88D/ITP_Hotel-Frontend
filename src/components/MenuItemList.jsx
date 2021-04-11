1d93665293c19a176d690d2379be3d0def673cdf
menaka809 <malithjayasekara2255721@gmail.com>
2021-04-11 17:33:46 +0530

    created menu item list component

---
+++ src/components/MenuItemList.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class MenuItemList extends Component {
+    constructor(props){
+        super(props)
+
+        this.state= {
+                menuItems:[]
+        }
+    }
+    render() {
+        return (
+            <div>
+                
+            <h2 className="text-center">Menu Item List</h2>
+             <div className="row">
+                <table className="table table-striped table-bordered">
+
+                    <thead>
+
+                        <tr>
+                           <th>Menu Item Type </th> 
+                           <th>Menu Item Name </th> 
+                           <th>Unit Price </th> 
+                           <th>Description </th> 
+                           <th>Discount </th> 
+                           <th>Available Status </th> 
+                           <th>Edit </th>
+                        </tr>
+
+                    </thead>
+
+                    <tbody>
+
+                        {
+                            this.state.menuItems.map(
+                                menuitem =>
+                                <tr key={menuitem.id}>
+                                    <td>{menuitem.menuItemType}</td>
+                                    <td>{menuitem.menuItemName}</td>
+                                    <td>{menuitem.unitPrice}</td>
+                                    <td>{menuitem.description}</td>
+                                    <td>{menuitem.discount}</td>
+                                    <td>{menuitem.availableStatus}</td>
+
+
+
+
+                                </tr>
+
+                            )
+                        }
+
+                    </tbody>
+
+
+                </table>
+
+            </div>
+
+            </div>
+        );
+    }
+}
+
+export default MenuItemList;
