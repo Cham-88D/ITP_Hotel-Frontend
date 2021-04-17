b4b90bc63545def6566535d8d388b9add2144a4c
Malindu Jethaka <malindujethaka@gmail.com>
2021-04-17 08:41:59 +0530

    salary function file making

--- src/pages/ManageSalary.js
+++ src/pages/ManageSalary.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import ViewAttendance from '../components/ViewAttendance'
+
 
 
 
 function Manage_Salary() {
     return (
         <div >
-          <ViewAttendance/>
+          
         </div>
     )
 }
