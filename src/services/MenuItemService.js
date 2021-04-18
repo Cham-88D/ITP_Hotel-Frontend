a00015666da34bf49dedb24e84921dfcd04a715e
menaka809 <malithjayasekara2255721@gmail.com>
2021-04-18 14:43:16 +0530

    created add menu type service

--- src/services/MenuItemService.js
+++ src/services/MenuItemService.js
@@ -4,9 +4,15 @@ import axios from'axios';
 
 const Menu_Item_Base_URL= "http://localhost:8080/api/v1/res_menu_item";
 
+const Menu_Type_Base_URL= "http://localhost:8080/api/v1/menu_item_category";
+
 class MenuItemService  {
    getMenuItems(){
        return axios.get(Menu_Item_Base_URL);
+   }
+    
+   addMenuType(menuType){
+    return axios.post(Menu_Type_Base_URL, menuType);
    }
 
    addMenuItem(menuItem){
@@ -16,6 +22,24 @@ class MenuItemService  {
    getMenuItemById(menuItemId){
             return axios.get(Menu_Item_Base_URL + '/' + menuItemId);
    }
+
+   updateMenu(menuItem,menuItemId){
+        return axios.put(Menu_Item_Base_URL + '/' +menuItemId,menuItem);
+   }
+
+   deleteMenu(menuItemId){
+       return axios.delete(Menu_Item_Base_URL + '/' + menuItemId);
+   }
+
+   getMenuTypeById(){
+       let x = axios.get(Menu_Type_Base_URL)
+       return x;
+       console.log(x);
+    
+    
+   }
+  
+
 }
 
 export default new MenuItemService()
