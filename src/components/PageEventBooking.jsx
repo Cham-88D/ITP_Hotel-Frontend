8c2b6f6c0bb52d256f682c2d17cbcda1a1eece3f
MiharaEk <mipinuri98@gmail.com>
2021-05-15 00:09:57 +0530

    Commit Event Booking Page

---
+++ src/components/PageEventBooking.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Event.css';
+
+class PageEventBooking extends Component {
+    render() {
+        return (
+            <div className="container">
+                 <h2 className="tableheading"> Event Bookings Page</h2> 
+                        <div id="EventBookingPage">
+                            <p>Welcome to the Event Booking Page of Villa Acres Green</p>
+                        </div>
+                    
+            </div>
+        );
+    }
+}
+
+export default PageEventBooking;
