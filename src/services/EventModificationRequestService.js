28d8f042fd4d9cd4be334741d7f46dca9b57b0ad
MiharaEk <mipinuri98@gmail.com>
2021-05-15 00:06:58 +0530

    Commit Event Modification Request Components

---
+++ src/services/EventModificationRequestService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const EVENT_MODIFICATION_REQUEST_API_BASE_URL="http://localhost:8080/api/v1/ev_Modification_Requests";
+
+class EventModificationRequestService {
+    getAllEv_Modification_Requests(){
+        //return axios.post(EVENT_MODIFICATION_REQUEST_API_BASE_URL);
+        let x = axios.get(EVENT_MODIFICATION_REQUEST_API_BASE_URL);
+        console.log(x);
+        return x;
+    
+    }
+    createEv_Modification_Request(eventModificationRequests){
+        // console.log(eventModificationRequest)
+        return axios.post(EVENT_MODIFICATION_REQUEST_API_BASE_URL, eventModificationRequests);
+    }
+    getEv_Modification_RequestById(Ev_M_ID){
+        return axios.get(EVENT_MODIFICATION_REQUEST_API_BASE_URL + '/' + Ev_M_ID);
+    }
+}
+export default new EventModificationRequestService()
