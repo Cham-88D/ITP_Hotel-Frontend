8e370f0858e2ff866698fc85f2cea4998a61311f
Malindu Jethaka <malindujethaka@gmail.com>
2021-05-16 21:13:02 +0530

    add new form

--- src/components/AddAttendForm.jsx
+++ src/components/AddAttendForm.jsx
@@ -1,22 +1,50 @@
 
 import React, { Component } from 'react'
 import AttendanceService from '../adapters/AttendanceService';
+import '../styles/AddAttend.css';
 
+const options = [
+    { value: 'flavor', label: 'flavor' },
+    { value: 'yummy', label: 'yummy' },
+    { value: 'red', label: 'red' },
+    { value: 'green', label: 'green' },
+    { value: 'yellow', label: 'yellow' },
+];
+
+const initialState={
+    
+    employee:{id:''},
+    in_Time:'',
+    out_Time:'',
+    otHours:'',
+    atte_type:'',
+    in_Time_Error:'',
+    out_Time_Error:'',
+    otHours_Error:'',
+    atte_type_Error:'',
+  
+    
+    
+
+}
 
 
  class AddAttendForm extends Component {
-     
+    interval = null;
+    attendanceId = null;
      constructor(props){
          super(props)
 
          this.state={
+             initialState,
+             attendance:[],
+             final_calulation:[],
+             selectedOption: 'txt',
             
-            employee:{id:''},
-            in_Time:'',
-            out_Time:'',
-            otHours:'',
-            atte_type:''
-         }
+            }
+            
+           
+         
          
          this.changeEmployeeIdHandler=this.changeEmployeeIdHandler.bind(this);
          this.changeInTimeHandler=this.changeInTimeHandler.bind(this);
@@ -24,21 +52,69 @@ import AttendanceService from '../adapters/AttendanceService';
          this.changeOTHourHandler=this.changeOTHourHandler.bind(this);
          this.changeTypeHandler=this.changeTypeHandler.bind(this);
          this.saveAttendance=this.saveAttendance.bind(this);
+        //  this.cancel=this.cancel.bind(this);
+         
 
      }
-     
 
+     validate=()=>{
+       let in_Time_Error='';
+       let out_Time_Error='';
+       let otHours_Error='';
+       let atte_type_Error='';
+
+       if(!this.state.in_Time.includes(':')){
+           in_Time_Error="invalid type"
+       }
+       
+       if(!this.state.in_Time){
+           in_Time_Error="In Time is required"
+       }
+       if(!this.state.out_Time.includes(':')){
+            out_Time_Error="invalid type"
+       }
+        if(!this.state.out_Time){
+            out_Time_Error="Out Time is required"
+        }
+        if(!this.state.otHours){
+            otHours_Error="OT Hour is required"
+        }
+        if(this.state.otHours>5){
+            otHours_Error="OT Hour Should be less than 5 hours"
+        }
+        // if(!this.state.otHours.includes([1-4])){
+        //     otHours_Error="OT Hour Should be a number"
+        // }
+        if(!this.state.atte_type){
+            atte_type_Error="Attendance Type required"
+        }
+       if(in_Time_Error||out_Time_Error||otHours_Error||atte_type_Error){
+           this.setState({in_Time_Error,out_Time_Error,otHours_Error,atte_type_Error});
+           return false;
+       }
+
+       return true;
+     };
+
+   
      saveAttendance=(e)=>{
         e.preventDefault();
-        let attendance={atte_type:this.state.atte_type,in_Time:this.state.in_Time,otHours:this.state.otHours,
-         out_Time:this.state.out_Time,employee:{id:this.state.employee.id} };
-        console.log('attendance=>'+JSON.stringify(attendance));
-        AttendanceService.insertAttendance(attendance).then(res=>{
-           
-                
-                //  this.props.history.push('/manageSalary');
-        });
+        const isValid=this.validate();
+        if(isValid){
 
+            let attendance={atte_type:this.state.atte_type,in_Time:this.state.in_Time,otHours:this.state.otHours,
+                out_Time:this.state.out_Time,employee:{id:this.state.employee.id} };
+               console.log('attendance=>'+JSON.stringify(attendance));
+               this.setState(initialState);
+               AttendanceService.insertAttendance(attendance).then(res=>{
+                  
+                       
+                     this.props.history.push('/manageAttendance');
+               });
+       
+
+        }
+        
     }
      changeEmployeeIdHandler=(event)=>{
          this.setState({employee:{id:event.target.value}});
@@ -55,24 +131,107 @@ import AttendanceService from '../adapters/AttendanceService';
     changeTypeHandler=(event)=>{
         this.setState({atte_type:event.target.value});
     }
+   cancel(){
+       this.props.history.push('/manageAttendance');
+   }
+   
+   componentDidMount(){
+    AttendanceService.getAllDaily_Attendance().then((res)=>{
+        this.setState({attendances:res.data});
+        this.attendanceId = res.data;
+
+        var itemsets =  setTimeout(this.temp, 3000);
+        console.log('start');
+        
+        console.log(itemsets);
+        
+        console.log('end');
+
+    });
+
+   
+}
+handleChange = ({ target }) => {
+    var value_select_option = target.value;
+    alert(value_select_option);
+    this.setState({
+        selectedOption: target.value,
+    });
+}
+
+    temp=()=>{
+        alert("hhhh");
+
+        var atteId = this.attendanceId;
+        var id={};
+        var finalid=[];
+
+        for (let i = 0; i < atteId.length; i++) {
+            console.log(atteId[i]);
+           var ab =atteId[i]['attendanceId'];
+
+             var object = {
+                 'employe_id' :  id,
+            };
+            id[i] = object;
+            finalid[i]=object;
+                
+        }
+        console.log(id);
+        this.setState({final_calulation:id});
+
+        this.setState({attendance:id});
+        console.log(this.setState.selectedOption);
+        this.setState({selectedOption:'None 2'});
+         console.log(this.setState.selectedOption);
+        return;
+    }
+
+
     render() {
+      
+       
+        // const {times} = this.state;
         return (
+            
             <div className="form-container">
+
+            
                 <div className="form-add-sal">
-                <form className="form" >
+                <form className="form"  id="addAttend" >
                 <h3>Add Attendance</h3>
+                
+               
                     <div className="form-inputs">
                         <label htmlFor="employeeId" className='form-label'>
                         Employee ID
                         </label>
-                        <input 
+                        <select 
                             id='employeeId'
                             type="text" 
-                            name='id'
+                            name='employeeId'
                             className='form-input'
                             placeholder='Employee ID'
-                            value={this.state.employee.id}
-                            onChange={this.changeEmployeeIdHandler}/>
+                            // value={this.state.employee.id}
+                            onChange={this.changeEmployeeIdHandler}>
+                                
+                                    
+                                <option value ="CHOOSE"></option>
+                                <option value ="1">1</option>
+
+                                {/* {
+                                    this.state.attendance.map(type=>( <option key={attendance.id} >
+                                        {attendance.employee.id}
+                                    </option>
+                                    
+                                    ))
+                                } */}
+                                           
+
+                         </select>
+                               
+                    
+                            
                             {/* {errors.employeeId && <p>{errors.employeeId}</p>} */}
                     </div>
                     
@@ -86,9 +245,12 @@ import AttendanceService from '../adapters/AttendanceService';
                             type="text" 
                             name='in_Time'
                             className='form-input'
-                            placeholder='In Time'
+                            placeholder='HH:MM'
                             value={this.state.in_Time}
                             onChange={this.changeInTimeHandler}/>
+                            <div className='inputError'>
+                                {this.state.in_Time_Error}
+                            </div>
                             {/* {errors.inTime && <p>{errors.inTime}</p>} */}
                     </div>
                     <div className="form-inputs">
@@ -100,9 +262,12 @@ import AttendanceService from '../adapters/AttendanceService';
                             type="text" 
                             name='out_Time'
                             className='form-input'
-                            placeholder='Out Time'
+                            placeholder='HH:MM'
                             value={this.state.out_Time}
                             onChange={this.changeOutTimeHandler}/>
+                            <div className='inputError'>
+                                {this.state.out_Time_Error}
+                            </div>
                             {/* {errors.outTime && <p>{errors.outTime}</p>} */}
                     </div>
                     <div className="form-inputs">
@@ -117,25 +282,39 @@ import AttendanceService from '../adapters/AttendanceService';
                             placeholder='OT Hour'
                             value={this.state.otHours}
                             onChange={this.changeOTHourHandler}/>
+                            <div className='inputError'>
+                                {this.state.otHours_Error}
+                            </div>
                             {/* {errors.otHour && <p>{errors.otHour}</p>} */}
                     </div>
                     <div className="form-inputs">
                         <label htmlFor="type" className='form-label'>
                             Type
                         </label>
-                        <input 
+                        <select 
                             id='type'
                             type="text" 
                             name='atte_type'
                             className='form-input'
                             placeholder='Type'
                             value={this.state.atte_type}
-                            onChange={this.changeTypeHandler}/>
+                            onChange={this.changeTypeHandler}>
+                                <option value ="CHOOSE">Choose</option>
+                                <option value ="1">1</option>
+                                <option value ="ab">ab</option>
+                                <option value ="half-day">half-day</option>
+                                <option value ="Quater-day">Quater-day</option>
+                            
+                        </select>
+
+                            <div className='inputError'>
+                                {this.state.atte_type_Error}
+                            </div>
                             {/* {errors.type && <p>{errors.type}</p>} */}
                     </div>
                     <div >
                         <button className='form-input-btn1' onClick={this.saveAttendance} type='submit'>Save </button>
-                        <button className='form-input-btn2' type='reset'>Reset</button>
+                        <button className='form-input-btn2' onClick={this.cancel.bind(this)} type='reset'>Cancel</button>
 
                     </div>
                     
--- src/components/AddSalDetailForm.jsx
+++ src/components/AddSalDetailForm.jsx
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
 import SalaryServices from '../adapters/SalaryServices';
+import '../styles/AddSalary.css';
 
+const initialState={
+    employee:{id:''},
+    role:'',
+    ot_rate:'',
+    allowance:'',
+    etf:'',
+    epf:'',
+    basic:'',
+    role_Error:'',
+    ot_rate_Error:'',
+    allowance_Error:'',
+    etf_Error:'',
+    epf_Error:'',
+    basic_Error:'',
+
+
+}
  class AddSalDetailForm extends Component {
     constructor(props){
         super(props)
 
-        this.state={
-           
-           employee:{id:''},
-           role:'',
-           ot_rate:'',
-           allowance:'',
-           etf:'',
-           epf:'',
-           basic:'',
+        this.state=initialState;
 
 
-           
-        }
+
+
         this.changeEmployeeIdHandler=this.changeEmployeeIdHandler.bind(this);
         this.changeRoleHandler=this.changeRoleHandler.bind(this);
         this.changeBasicHandler=this.changeBasicHandler.bind(this);
@@ -29,13 +39,49 @@ import SalaryServices from '../adapters/SalaryServices';
 
     }
 
+    validate=()=>{
+    // let  role_Error='';
+    let  ot_rate_Error='';
+    let  allowance_Error='';
+    let  etf_Error='';
+    let  epf_Error='';
+    let  basic_Error='';
+
+    if(!this.state.ot_rate){
+        ot_rate_Error="In Time is required"
+    }
+    if(!this.state.allowance){
+        allowance_Error="Allowance is required"
+    }
+    if(!this.state.etf){
+        etf_Error="ETF is required"
+    }
+    if(!this.state.epf){
+        epf_Error="EPF is required"
+    }
+    if(!this.state.basic){
+        basic_Error="Basic is required"
+    }
+    if(ot_rate_Error||allowance_Error||etf_Error|| epf_Error||basic_Error){
+        this.setState({ot_rate_Error,allowance_Error,etf_Error, epf_Error,basic_Error});
+        return false;
+    }
+
+    return true;
+  };  
+ 
+
     saveSalaryDetail=(e)=>{
         e.preventDefault();
-        let salaryDetail={role:this.state.role,ot_rate:this.state.ot_rate,allowance:this.state.allowance,basic:this.state.basic,etf:this.state.etf,epf:this.state.epf,employee:{id:this.state.employee.id}};
-        console.log('salaryDetail=>'+JSON.stringify(salaryDetail));
-        SalaryServices.insertSalaryDetail(salaryDetail).then(res=>{
+        const isValid=this.validate();
+        if(isValid){
+            let salaryDetail={role:this.state.role,ot_rate:this.state.ot_rate,allowance:this.state.allowance,basic:this.state.basic,etf:this.state.etf,epf:this.state.epf,employee:{id:this.state.employee.id}};
+            console.log('salaryDetail=>'+JSON.stringify(salaryDetail));
+            SalaryServices.insertSalaryDetail(salaryDetail).then(res=>{
 
-        });
+                this.props.history.push('/manageSalary');
+            });
+        }
         
     }
 
@@ -61,23 +107,25 @@ import SalaryServices from '../adapters/SalaryServices';
         this.setState({epf:event.target.value});
     }
     
-    
+    cancel(){
+        this.props.history.push('/manageSalary');
+    }
     
     render() {
         return (
-                <div className="form-container">
-                    <div className="form-add-sal">
+                <div className="formcontainer">
+                    <div className="formaddsal">
                     <form className="form" >
-                    <h3>Add Attendance</h3>
-                        <div className="form-inputs">
-                            <label htmlFor="employeeId" className='form-label'>
+                    <h3 className='caption'>Add Salary Details</h3>
+                        <div className="forminputs">
+                            <label htmlFor="employeeId" className='formlabel'>
                             Employee ID
                             </label>
                             <input 
                                 id='employeeId'
                                 type="text" 
                                 name='id'
-                                className='form-input'
+                                className='forminput'
                                 placeholder='Employee ID'
                                 value={this.state.employee.id}
                                 onChange={this.changeEmployeeIdHandler}/>
@@ -85,21 +133,24 @@ import SalaryServices from '../adapters/SalaryServices';
                         </div>
                         
                         
-                        <div className="form-inputs">
-                            <label htmlFor="inTime" className='form-label'>
+                        <div className="forminputs">
+                            <label htmlFor="inTime" className='formlabel'>
                             Role
                             </label>
                             <input 
                                 id='role'
                                 type="text" 
                                 name='role'
-                                className='form-input'
+                                className='forminput'
                                 placeholder='Role'
                                 value={this.state.role}
                                 onChange={this.changeRoleHandler}/>
+                                <div className='input_Error'>
+                                    {this.state.role_Error}
+                                </div>
                                 {/* {errors.inTime && <p>{errors.inTime}</p>} */}
                         </div>
-                        <div className="form-inputs">
+                        <div className="forminputs">
                             <label htmlFor="outTime" className='form-label'>
                                 Basic
                             </label>
@@ -107,71 +158,86 @@ import SalaryServices from '../adapters/SalaryServices';
                                 id='basic'
                                 type="text" 
                                 name='basic'
-                                className='form-input'
+                                className='forminput'
                                 placeholder='Basic'
                                 value={this.state.basic}
                                 onChange={this.changeBasicHandler}/>
+                                <div className='input_Error'>
+                                    {this.state.basic_Error}
+                                </div>
                                 {/* {errors.outTime && <p>{errors.outTime}</p>} */}
                         </div>
-                        <div className="form-inputs">
-                            <label htmlFor="otHour" className='form-label'>
+                        <div className="forminputs">
+                            <label htmlFor="otHour" className='formlabel'>
                                 OT Rate
                             </label>
                             <input 
                                 id='otRate'
                                 type="text" 
                                 name='ot_rate'
-                                className='form-input'
+                                className='forminput'
                                 placeholder='OT Rate'
                                 value={this.state.ot_rate}
                                 onChange={this.changeOTRateHandler}/>
+                                <div className='input_Error'>
+                                    {this.state.ot_rate_Error}
+                                </div>
                                 {/* {errors.otHour && <p>{errors.otHour}</p>} */}
                         </div>
-                        <div className="form-inputs">
-                            <label htmlFor="type" className='form-label'>
+                        <div className="forminputs">
+                            <label htmlFor="type" className='formabel'>
                                 EPF
                             </label>
                             <input 
                                 id='epf'
                                 type="text" 
                                 name='epf'
-                                className='form-input'
+                                className='forminput'
                                 placeholder='EPF'
                                 value={this.state.epf}
                                 onChange={this.changeEPFHandler}/>
+                                 <div className='input_Error'>
+                                    {this.state.epf_Error}
+                                </div>
                                 {/* {errors.type && <p>{errors.type}</p>} */}
                         </div>
-                        <div className="form-inputs">
-                            <label htmlFor="type" className='form-label'>
+                        <div className="forminputs">
+                            <label htmlFor="type" className='formlabel'>
                                 ETF
                             </label>
                             <input 
                                 id='etf'
                                 type="text" 
                                 name='etf'
-                                className='form-input'
+                                className='forminput'
                                 placeholder='ETF'
                                 value={this.state.etf}
                                 onChange={this.changeETFHandler}/>
+                                 <div className='input_Error'>
+                                    {this.state.etf_Error}
+                                </div>
                                 {/* {errors.type && <p>{errors.type}</p>} */}
                         </div>
-                        <div className="form-inputs">
-                            <label htmlFor="type" className='form-label'>
+                        <div className="forminputs">
+                            <label htmlFor="type" className='formlabel'>
                                 Allowance
                             </label>
                             <input 
                                 id='allowance'
                                 type="text" 
                                 name='allowance'
-                                className='form-input'
+                                className='forminput'
                                 placeholder='Allowance'
                                 value={this.state.allowance}
                                 onChange={this.changeAllowanceHandler}/>
+                                 <div className='input_Error'>
+                                    {this.state.allowance_Error}
+                                </div>
                                 {/* {errors.type && <p>{errors.type}</p>} */}
                         </div>
                         <div >
-                            <button className='form-input-btn1' onClick={this.saveSalaryDetail} type='submit'>Save </button>
-                            <button className='form-input-btn2' type='reset'>Reset</button>
+                            <button className='forminputbtn1' onClick={this.saveSalaryDetail} type='submit'>Save </button>
+                            <button className='forminputbtn2' onClick={this.cancel.bind(this)} type="reset">Cancel</button>
     
                         </div>
                         
