c5ca1c86b65b298efabb9281a3fea2e0b58ddcfa
Minuli-Lavanya <minulilavanya@gmail.com>
2021-05-18 15:45:29 +0530

    change food count report

--- src/components/FoodCountReport-IT19176116.js
+++ src/components/FoodCountReport-IT19176116.js
@@ -9,8 +9,7 @@ export default class example extends Component {
                 <div>
               <ReactToPrint
                 trigger={() => {
-                  // NOTE: could just as easily return <SomeComponent />. Do NOT pass an `onClick` prop
-                  // to the root node of the returned component as it will be overwritten.
+                  
                   return  <button style={{marginLeft:855, marginTop:50}} className="btn btn-danger" onClick={this.generatepdf} type='submit'>Generate PDF</button>
                 }}
                 content={() => this.componentRef}
